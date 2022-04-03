788b9042942f226488e2c8bd2a8e40b471102e4d
kevinlau04 <101075114+kevinlau04@users.noreply.github.com>
2022-04-02 22:06:22 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-let nav = document.querySelector
+var box = document.querySelectorAll(".box");
+
+for (i=0; i<box.length;i++){
+    box[i].addEventListener("click", function(){
+        this.classList.toggle("active");
+    })
+}
